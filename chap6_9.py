fd60b18bcb42cb8652e75fff5e4a1ab7cc0a0999
Muhammad Arsalan <arsalan.9798@gmail.com>
2020-08-27 11:08:12 +0500

    6.15 glove trained model with weights

--- chap6_9.py
+++ chap6_9.py
@@ -56,8 +56,8 @@ embeddings_index={}
 file_glove=open(os.path.join(glove_dir,'glove.6B.100d.txt'),encoding="utf8") #open file
 for line in file_glove: 
     values=line.split() #split after each line in list 
-    word=values[0] #the first valve of list is the word
-    coefs=np.asarray(values[1:],dtype='float32') #next values in list is the vector 
+    word=values[0] #the first value of list is the word
+    coefs=np.asarray(values[1:],dtype='float32') #next values in list is the vector 100 words
     embeddings_index[word]=coefs #saved the word and vector as key value pair, 400000 words
 file_glove.close()
 
@@ -66,5 +66,52 @@ file_glove.close()
 # print(values)
 # print(word)
 
+#6.11 preparing matrix for glove 
+embedding_dim =100 #total vector length for each word
+embedding_matrix = np.zeros((maxwords,embedding_dim)) #empty matrix of 10000,100
+#10000 words for embedding space and 100 dimensions per word by glove
+for word, i in word_index.items(): #item returns word and integer , enrich 24436, strayer 24437, vampiric 24438
+    #print(word,i)
+    if i< maxwords:
+        embedding_vector= embeddings_index.get(word) #embeddings_index is dict words and vectors
+        if embedding_vector is not None:
+            embedding_matrix[i] = embedding_vector  #10000 words and 100 dimensions created in a matrix
 
+#print(embedding_matrix[2:3,:]) #array sliced for 2nd row and all 100 columns
+
+#6.12 model 
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.layers import Embedding, Flatten, Dense
+model= Sequential()
+model.add(Embedding(maxwords,embedding_dim,input_length=maxlen))
+model.add(Flatten())
+model.add(Dense(32,activation="relu"))
+model.add(Dense(1,activation='sigmoid'))
+#print(model.summary())
+
+#stop traing of embedding layer , use glove trained matrix
+model.layers[0].set_weights([embedding_matrix])
+model.layers[0].trainable=False
+
+model.compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['acc'])
+history=model.fit(x_train,y_train,epochs=10,batch_size=32,validation_data=(x_val,y_val))
+model.save_weights('pre_trained_glove_model.h5')
+
+#importing matplotlib for visualization
+import matplotlib.pyplot as plt
+acc=history.history['acc']
+val_acc=history.history['val_acc']
+loss=history.history['loss']
+val_loss=history.history['val_loss']
+epochs=range(1,len(acc)+1)
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
 
